6e47feb9747db4402ba082413431ebead00974f8
Philippe MICHEL <philippe.michel1@gmail.com>
2021-07-26 21:45:47 +0200

    Initial Commit

---
+++ PositionUtilisateur/PositionUtilisateurApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositionUtilisateurApp.swift
+//  PositionUtilisateur
+//
+//  Created by Philippe MICHEL on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct PositionUtilisateurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
